81ce7d707a2236404a854f913ad11eadafd96e8e
hemanth6768 <ghr6768@gmail.com>
2026-03-11 21:44:08 +0530

    added the azure sql sevrver url

--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = ( 
-    "mssql+pyodbc://ghr:8888@localhost/Radiusmart"
-    "?driver=ODBC+Driver+17+for+SQL+Server"
-)
+# DATABASE_URL = ( 
+#     "mssql+pyodbc://ghr:8888@localhost/Radiusmart"
+#     "?driver=ODBC+Driver+17+for+SQL+Server"
+# )
 
+DATABASE_URL = (
+    "mssql+pyodbc://hemanth:Br3Zf6kcf1!P@radiusmart.database.windows.net:1433/Radiusmart"
+    "?driver=ODBC+Driver+18+for+SQL+Server"
+    "&Encrypt=yes"
+    "&TrustServerCertificate=no"
+)
 
 engine = create_engine(DATABASE_URL, echo=True)
 
